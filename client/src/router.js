214acb527b0b9b0d6104597f45533150ed54050f
aburbanol <andres.a.burbano@gmail.com>
2020-10-14 15:53:11 +0200

    initial commit

--- client/src/router.js
+++ client/src/router.js
@@ -1,6 +1,9 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
+import Orders from "./views/Orders.vue";
+import Dashboard from "./views/Dashboard.vue";
+import Servicecontracts from "./views/Servicecontracts.vue";
 
 Vue.use(Router);
 
@@ -17,6 +20,21 @@ export default new Router({
       path: "/about",
       name: "about",
       component: () => import("./views/About.vue")
+    },
+    {
+      path: "/orders",
+      name: "orders",
+      component: Orders
+    },
+    {
+      path: "/dashboard",
+      name: "dashboard",
+      component: Dashboard
+    },
+    {
+      path: "/servicecontracts",
+      name: "servicecontracts",
+      component: Servicecontracts
     }
   ]
 });
